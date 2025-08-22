75932e4830b75a31dda5363472774414f32c8953
v605690 <v605690@gmail.com>
2025-08-21 21:16:02 -0500

    Add another custom exception - CategoryRepository.java, CategoryServiceImpl.java

--- src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CategoryRepository.java
@@ -3,8 +3,11 @@ package com.ecommerce.project.repositories;
 import com.ecommerce.project.model.Category;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+    Category findByCategoryName(String categoryName);
 }
