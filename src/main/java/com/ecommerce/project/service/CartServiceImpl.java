38e303462c0f80151d6d5716fbeb11ec448f870c
v605690 <v605690@gmail.com>
2026-03-06 14:36:10 -0600

    add and update cart item, CartItemRepository.java, CartServiceImpl.java, AuthUtil.java

--- src/main/java/com/ecommerce/project/service/CartServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CartServiceImpl.java
@@ -1,16 +1,37 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.dto.CartDTO;
+import com.ecommerce.project.dto.ProductDTO;
+import com.ecommerce.project.exceptions.APIException;
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Cart;
+import com.ecommerce.project.model.CartItem;
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.repositories.CartItemRepository;
 import com.ecommerce.project.repositories.CartRepository;
+import com.ecommerce.project.repositories.ProductRepository;
+import com.ecommerce.project.util.AuthUtil;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 public class CartServiceImpl implements CartService {
 
     @Autowired
     CartRepository cartRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    private CartItemRepository cartItemRepository;
+
+    @Autowired
+    ModelMapper modelMapper;
 
     @Autowired
     AuthUtil authUtil;
@@ -20,13 +41,68 @@ public class CartServiceImpl implements CartService {
 
         // Find an existing cart or create one
         Cart cart = createCart();
-        
+
         // Retrieve Product Details
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
+
         // Perform Validations
+        CartItem cartItem = cartItemRepository.findCartItemByProductProductIdAndCartId(cart.getCartId(), productId);
+
+        if (cartItem != null) {
+            throw new APIException("Product " + product.getProductName() + "already exists in the cart");
+        }
+        if (product.getQuantity() == 0) {
+            throw new APIException(product.getProductName() + " is not available");
+        }
+        if (product.getQuantity() < quantity) {
+            throw new APIException("Please, make an order of the " + product.getProductName() + " less than or equal to the quantity " +
+                    product.getQuantity() + ".");
+        }
+
         // Create Cart Item
+        CartItem newCartItem = new CartItem();
+
+        newCartItem.setProduct(product);
+        newCartItem.setCart(cart);
+        newCartItem.setQuantity(quantity);
+        newCartItem.setDiscount(product.getDiscount());
+        newCartItem.setProductPrice(product.getSpecialPrice());
+
         // Save Cart Item
+        cartItemRepository.save(newCartItem);
+        product.setQuantity(product.getQuantity());
+        cart.setTotalPrice(cart.getTotalPrice() + (product.getSpecialPrice() * quantity));
+
+        cartRepository.save(cart);
+
         // Return update cart
-        return null;
+        // convert cart to CartDTO
+        CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
+
+        // get data
+        // -> stream it
+        // -> map it
+        // -> collect to list
+        // -> store in dto
+
+        // get cart items
+        // transform each CartItem to ProductDTO
+        // add the quantity
+        // return productDTO
+        // convert the result into a list
+        // put that list into the CartDTO
+        // return cartDTO
+        List<ProductDTO> productDTOS = cart.getCartItems().stream()
+                .map(item -> {
+                    ProductDTO productDTO = modelMapper.map(item.getProduct(), ProductDTO.class);
+                    productDTO.setQuantity(item.getQuantity());
+                    return productDTO;
+                })
+                .toList();
+
+        cartDTO.setProducts(productDTOS);
+        return cartDTO;
     }
 
     private Cart createCart() {
@@ -35,7 +111,7 @@ public class CartServiceImpl implements CartService {
         if (userCart != null) {
             return userCart;
         }
-        // if the cart is not found, create new cart
+        // if the cart is not found, create a new cart
         Cart cart = new Cart();
         cart.setTotalPrice(0.00);
         cart.setUser(authUtil.loggedInUser());
